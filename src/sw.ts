ebd9c17e4f77d879043cc03487711af7c76d72f4
Luifer1994 <almendralesluifer@gmail.com>
2024-06-14 00:38:12 -0500

    PWA

--- src/sw.ts
+++ src/sw.ts
@@ -1,7 +1,8 @@
-import { precacheAndRoute } from 'workbox-precaching'
-declare let self: ServiceWorkerGlobalScope
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting()
-})
+import { precacheAndRoute } from 'workbox-precaching';
+declare let self: ServiceWorkerGlobalScope;
 
-precacheAndRoute(self.__WB_MANIFEST)
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
+
+precacheAndRoute(self.__WB_MANIFEST);
